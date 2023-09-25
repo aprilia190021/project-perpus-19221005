bea5a59058958537b4a18945b8ba3a986ad890d2
unknown <silfiasilfiavia@gmail.com>
2023-09-25 20:54:23 +0700

    menmbah fitur kategori

---
+++ app/Controllers/KategoriController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+Class Kategoricontroller extends BaseController
+{
+    public function daftar()
+    {
+
+    }
+    public function nama()
+    {
+
+    }
+    public function deskripsi()
+    {
+
+    }
+}
