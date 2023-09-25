f1ff5533e849a62b6909474999b59a0af1163568
tasa19220993 <bakistatasa@gmail.com>
2023-09-25 18:36:18 +0700

    menambah fitur login

---
+++ app/Controllers/LoginControllers.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Kategoricontroller extends BaseController
+{
+    public function username ()
+    {
+
+    }
+    public function masukkan_password ()
+    {
+
+    }
+    public function get_started ()
+    {
+
+    }
+    public function lupa_password ()
+    {
+        
+    }
+}
