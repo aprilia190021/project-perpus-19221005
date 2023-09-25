9cffd5cf3b91d9941f1863321fa475a429274a7f
tasa19220993 <bakistatasa@gmail.com>
2023-09-25 18:05:34 +0700

    menambah fitur kategori

---
+++ app/Controllers/KategoriController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Kategoricontroller extends BaseController
+{
+    public function daftar_kategori ()
+    {
+
+    }
+    public function nama_kategori ()
+    {
+
+    }
+    public function deskripsi ()
+    {
+
+    }
+}
+
+
