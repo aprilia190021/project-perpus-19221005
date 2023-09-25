1c76edcb48ca5dc54ebfe0ac962a41b14b7554eb
tasa19220993 <bakistatasa@gmail.com>
2023-09-25 18:22:25 +0700

    menambah fitur beranda

---
+++ app/Controllers/BukuControllers.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Kategoricontroller extends BaseController
+{
+    public function Judul_buku ()
+    {
+
+    }
+    public function nama_penerbit ()
+    {
+
+    }
+    public function tahun_rilis ()
+    {
+
+    }
+    public function sumber_buku()
+    {
+        
+    }
+}
