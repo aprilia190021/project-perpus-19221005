8eb66197df2377a6460f443d4fd3c8c27dba5385
Anjelina24 <angelinastg5555@gmail.com>
2023-09-25 04:52:20 -0700

    menambahkan fitur login

--- login.php
+++ login.php
@@ -2,37 +2,30 @@
 
 namespace App\Controllers;
 
-class Login extends BaseController
-{
-    public function index()
-    {
-        // Tampilkan halaman login
-        return view('login');
-    }
 
+class katagoricontroller extends BaseController
+{
     public function login()
     {
-        // Validasi input dari form login
-        $validation = \Config\Services::validation();
-        $validation->setRules([
-            'email' => 'required|valid_email',
-            'password' => 'required',
-        ]);
+        
+    }
 
-        if (!$validation->withRequest($this->request)->run()) {
-            // Jika validasi gagal, kembali ke halaman login
-            return redirect()->to('/login')->withInput()->with('errors', $validation->getErrors());
-        }
+    public function username()
+    {
+        
+    }
 
-        // Cek kredensial pengguna dan lakukan otentikasi
-        // Implementasi otentikasi akan bergantung pada metode Anda
+    public function masukan_password()
+    {
+        
+    }
 
-        if ($login_success) {
-            // Jika otentikasi berhasil, arahkan ke halaman yang sesuai
-            return redirect()->to('/dashboard');
-        } else {
-            // Jika otentikasi gagal, tampilkan pesan kesalahan
-            return redirect()->to('/login')->with('error', 'Email atau password salah.');
-        }
+    public function lupa_paswsword()
+    {
+        
+    }
+    public function get_started()
+    {
+
     }
 }
