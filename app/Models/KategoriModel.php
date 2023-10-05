14dd93ccaca09087c45f12beefe191cef40d0372
unknown <silfiasilfiavia@gmail.com>
2023-10-05 14:13:59 +0700

    database

---
+++ app/Models/KategoriModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModels;
+use CodeIgniter\BaseModel;
+
+class KategoriModels extends BaseModels
+{
+    public function index()
+    {
+        //
+    }
+    public function novel()
+    {
+        //
+    }
+    public function majalah()
+    {
+        //
+    }
+    public function kamus()
+    {
+        //
+    }
+}
