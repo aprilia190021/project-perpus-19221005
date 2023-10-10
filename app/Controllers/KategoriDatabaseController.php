5fb94cef7ecbb326b49fa766011c8347bd34eaff
tasa19220993 <bakistatasa@gmail.com>
2023-10-10 16:53:25 +0700

    database controller kategori

---
+++ app/Controllers/KategoriDatabaseController.php
@@ -0,0 +1,51 @@
+<?php
+// Koneksi ke database
+$servername = "sql"; 
+$database = "nama_database";
+
+$conn = new mysqli( $database);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// Fungsi untuk menambahkan kategori baru
+function tambahKategori($ide, $kode_ddc) {
+    global $conn;
+    $sql = "INSERT INTO kategori (ide, kode_ddc) VALUES ('$ide', '$kode_ddc')";
+    if ($conn->query($sql) === TRUE) {
+        return "Kategori berhasil ditambahkan.";
+    } else {
+        return "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Fungsi untuk mengambil daftar kategori
+function getDaftarKategori() {
+    global $conn;
+    $sql = "SELECT * FROM kategori";
+    $result = $conn->query($sql);
+    $kategori = array();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $kategori[] = $row;
+        }
+    }
+    return $kategori;
+}
+
+// Fungsi untuk menghapus kategori berdasarkan ID
+function hapusKategori($id) {
+    global $conn;
+    $sql = "DELETE FROM kategori WHERE id = $id";
+    if ($conn->query($sql) === TRUE) {
+        return "Kategori berhasil dihapus.";
+    } else {
+        return "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Tutup koneksi database
+$conn->close();
+?>
