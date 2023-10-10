a2a347e0aa0930d5dfa76b8fa5bcf5748b5714b5
tasa19220993 <bakistatasa@gmail.com>
2023-10-10 17:11:15 +0700

    peminjaman controller

---
+++ app/Controllers/PeminjamanController.php
@@ -0,0 +1,50 @@
+<?php
+class PeminjamanController extends PeminjamanController
+{
+    public function index()
+    {
+        // Logika untuk menampilkan daftar peminjaman
+    }
+
+    public function create()
+    {
+        // Logika untuk menampilkan form peminjaman
+    }
+
+    public function store( $request)
+    {
+        // Logika untuk menyimpan data peminjaman ke dalam database
+        $tgl_peminjaman = $request->input('tgl_peminjaman');
+        $tgl_pengembalian = $request->input('tgl_pengembalian');
+        $tb_penguna_id_pinjaman = $request->input('tb_penguna_id_pinjaman');
+        $tb_penguna_id_pengembalian = $request->input('tb_penguna_id_pengembalian');
+        $tb_anggota_id = $request->input('tb_anggota_id');
+        $tb_buku = $request->input('tb_buku');
+        $denda = $request->input('denda');
+
+        // Simpan data ke dalam database
+
+        // Redirect atau tampilkan pesan sukses
+    }
+
+    public function show($id)
+    {
+        // Logika untuk menampilkan detail peminjaman dengan ID tertentu
+    }
+
+    public function edit($id)
+    {
+        // Logika untuk menampilkan form edit peminjaman dengan ID tertentu
+    }
+
+    public function update( $request, $id)
+    {
+        // Logika untuk menyimpan perubahan data peminjaman ke dalam database
+    }
+
+    public function destroy($id)
+    {
+        // Logika untuk menghapus data peminjaman dengan ID tertentu
+    }
+}
+?>
